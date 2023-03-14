52f09a0bd639c4a1d4cc23b086f5e7ed6d9803b7
Goshargo <margot87900@gmail.com>
2023-03-14 13:35:43 +0100

    Initial commit

---
+++ test-site/scripts/main.js
@@ -0,0 +1,72 @@
+//Changing the image
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "/images/m.png") {
+    myImage.setAttribute("src", "/images/madzia.png");
+  } else {
+    myImage.setAttribute("src", "/images/m.png");
+  }
+};
+
+//Poking
+document.querySelector("html").addEventListener("click", function () {
+    alert("Ouch! Stop poking me!");
+  });
+
+//Name on the site
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+  }
+  
+
+  //if (!localStorage.getItem("name")) {
+    setUserName();
+  //} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+  //}
+
+  myButton.onclick = () => {
+    setUserName();
+  };
+  
+  
+/*
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+
+let iceCream = "chocolate";
+if (iceCream === "chocolate") {
+  alert("Yay, I love chocolate ice cream!");
+} else {
+  alert("Awwww, but chocolate is my favorite…");
+}
+
+
+let myVariable = document.querySelector("h1");
+alert("hello!");
+
+function multiply(num1, num2) {
+    let result = num1 * num2;
+    return result;
+  }
+
+document.write(multiply(4, 7));
+document.write("\n"+multiply(20, 20));
+document.write("\n"+multiply(0.5, 3));
+
+document.querySelector("html").addEventListener("click", () => {
+  alert("Ouch! Stop poking me!");
+});
+*/
